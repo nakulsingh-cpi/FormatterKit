779e8d02e34ca70577e5543a67b43cc2a1be62a4
Nakul Singh <nakul.singh@chargepoint.com>
2025-07-07 23:23:04 +0530

    initialized package.swift file

---
+++ Package.swift
@@ -0,0 +1,35 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "FormatterKit",
+    platforms: [
+        .iOS(.v9),
+        .macOS(.v10_10),
+        .tvOS(.v9),
+        .watchOS(.v2)
+    ],
+    products: [
+        .library(
+            name: "FormatterKit",
+            targets: ["FormatterKit"]
+        ),
+    ],
+    targets: [
+        .target(
+            name: "FormatterKit",
+            path: "Sources/FormatterKit",
+            publicHeadersPath: "include",
+            cSettings: [
+                .headerSearchPath("include"),
+                .define("SWIFT_PACKAGE")
+            ],
+            linkerSettings: [
+                .linkedFramework("Foundation"),
+                .linkedFramework("AddressBook", .when(platforms: [.iOS, .macOS])),
+                .linkedFramework("AddressBookUI", .when(platforms: [.iOS])),
+                .linkedFramework("CoreLocation")
+            ]
+        ),
+    ]
+)
